6c676f47ef4796bea3595f2cd87b338d40d1b097
Mohamed Mayla <mohamed.mayla@gmail.com>
2017-07-26 23:36:22 +0200

    Create user API

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,12 +1,10 @@
 /**
- * StoreController
+ * UserController
  *
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
 module.exports = {
-  find: function (req, res) {
-    return res.json(403, 'Single model lookup is denied.');
-  }
+  
 };
