08c54c01def8abde5b9e9e3ba1b309c5e86913c7
Mohamed Mayla <mohamed.mayla@gmail.com>
2017-07-26 19:28:28 +0200

    Create store and car apis

--- api/controllers/CarController.js
+++ api/controllers/CarController.js
@@ -1,11 +1,12 @@
 /**
- * StoreController
+ * CarController
  *
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
 module.exports = {
-
+  
 
 };
+
